e96bc4f27325682bd0fcd109b5c18737890d3088
Hasan Huda <hasanhuda037@gmail.com>
2023-05-11 16:21:10 -0400

    introduced typescript annotations

--- index.ts
+++ index.ts
@@ -2,6 +2,18 @@ import axios from 'axios';
 
 const url = 'http://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response=>{
-  console.log(response.data);
+  const todo=response.data as Todo;
+  const id=todo.id;
+  const completed=todo.completed;
+  const title=todo.title;
+
+  console.log(`id: ${id} \n title: ${title} \n completed: ${completed}`)
 });
+
